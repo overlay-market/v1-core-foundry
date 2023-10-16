84fc958e06c99a86500052b69a4220b4ee6e97e8
ionOM <ionblockchain@gmail.com>
2023-10-16 17:53:14 +0000

    modify FixedCastMoch.t.sol and add setup FixedPointMock

--- tests/libraries/fixedcast/FixedCastMoch.t.sol
+++ tests/libraries/fixedcast/FixedCastMoch.t.sol
@@ -23,54 +23,43 @@ contract FixedCastTest is Test {
     ///////////////////////
     //// test_views.py ////
     ///////////////////////
+
     function testToUint256Fixed(uint16 _value) public {
         // convert 4 places to 18 places
         uint256 expect = uint256(_value) * uint256(1e14);
         uint256 actual = fixedCast.toUint256Fixed(_value);
         assertEq(expect, actual);
     }
-
-    // ?????????????????????????????
+    
     function testToUint16Fixed(uint256 _value) public {
-        uint256 min_value = 1e6; // Equivalent to 0.000001 with 6 decimal places
-        uint256 max_value = 6553500; // Equivalent to 6.5535 with 4 decimal places
-        _value = bound(_value, min_value, max_value);
+        _value = bound(_value, 0.000001e18, 6.5535e18);
 
-        _value = _value * 1e12; // ???????????????
         uint256 expect = _value / 1e14;
         uint256 actual = fixedCast.toUint16Fixed(_value);
         assertEq(expect, actual);
     }
 
     function testToUint16Touint256Fixed(uint256 _value) public {
-        uint256 min_value = 1e6; // Equivalent to 0.000001 with 6 decimal places
-        uint256 max_value = 6553500; // Equivalent to 6.5535 with 4 decimal places
-        _value = bound(_value, min_value, max_value);
-
-        _value = _value * 1e12; // ???????????????
-        uint256 expect = (_value / 1e14) * 1e14; //?????????????
+        // convert 18 places to 4 places back to 18 places rounded down
+        _value = bound(_value, 0.000001e18, 6.5535e18);
+        uint256 expect = (_value / 1e14) * 1e14;
         uint256 actual = fixedCast.toUint256Fixed(fixedCast.toUint16Fixed(_value));
         assertEq(expect, actual);
-
     }
 
     function testToUint16FixedRevertsWhenGtMax() public {
         // Should pass for type(uint16).max
-           uint256 value = 2**16-1;
-           uint256 inputValue = value * 1e14;
-           uint256 expect = value;
-           uint16 actual = fixedCast.toUint16Fixed(inputValue);
-           assertEq(expect, actual);
+        uint256 value = 2**16-1;
+        uint256 inputValue = value * 1e14;
+        uint256 expect = value;
+        uint16 actual = fixedCast.toUint16Fixed(inputValue);
+        assertEq(expect, actual);
 
-           // Should fail for type(uint16).max + 1
-           value = 2**16;
-           inputValue = value * 1e14;
-           vm.expectRevert(bytes("OVLV1: FixedCast out of bounds"));
-           fixedCast.toUint16Fixed(inputValue);
-
-    
+        // Should fail for type(uint16).max + 1
+        value = 2**16;
+        inputValue = value * 1e14;
+        vm.expectRevert(bytes("OVLV1: FixedCast out of bounds"));
+        fixedCast.toUint16Fixed(inputValue);
     }
-
-
 }
 
--- tests/libraries/fixedpoint/FixedPointMock.t.sol
+++ tests/libraries/fixedpoint/FixedPointMock.t.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.10;
 import {Test} from "forge-std/Test.sol";
 import {console} from "forge-std/console.sol";
 import {FixedPointMock} from "contracts/mocks/FixedPointMock.sol";
+import {FixedPointMathLib} from "solady/src/utils/FixedPointMathLib.sol";
 
 contract FixedPointMockTest is Test {
 
@@ -18,6 +19,8 @@ contract FixedPointMockTest is Test {
 
     FixedPointMock public fixedPoint;
 
+    using FixedPointMathLib for uint256;
+
     function setUp() public {
         vm.prank(alice);
         fixedPoint = new FixedPointMock();
@@ -30,38 +33,28 @@ contract FixedPointMockTest is Test {
     // I don't know how to find an alternative in foundry for:
     //  "from math import exp" and from pytest import approx
 
-    /*function testExpUp(uint256 _x) public {
-        uint256 min_value = 0;
-        uint256 max_value = 40;
-        _x = bound(_x, min_value, max_value);
+    /* function testExpUp(uint256 _x) public {
+
+        _x = bound(_x, 0, 40);
 
         uint256 x_fixed = _x * 1e18;
 
-        uint256 expect = exp(_x) * 1e18;
+        uint256 expect = fixedPoint.expWad(_x); //???????
         uint256 actual = fixedPoint.expUp(x_fixed);
 
-        assertEq(expect, approx(actual));
+        //assertEq(expect, approx(actual));
         // lesser than or equal to
-        assertLe(exprct, actual); // check round up error added
+        assertLe(expect, actual); // check round up error added
 
     } */
 
-    function testExpUpWhenPowIsZero() public {
+    function test_exp_up_when_pow_is_zero() public {
         uint256 x = 0;
         uint256 expect = 1000000000000000000;
         uint256 actual = fixedPoint.expUp(x);
         assertEq(expect, actual);
     }
-
-    function testExpUpRevertsWhenXGreaterThanInt256() public {
-        // check reverts when greater than int256 max
-        uint256 x = 2**255;
-        vm.expectRevert(bytes("FixedPoint: x out of bounds"));
-        fixedPoint.expUp(x);
-
-
-    }
-
+   
     /*
     function testExpDown() public {
         uint256 min_value = 0;
@@ -69,18 +62,5 @@ contract FixedPointMockTest is Test {
         _x = bound(_x, min_value, max_value);
     } */
 
-    function testExpDownWhenPowIsZero() public {
-        uint256 x = 0;
-        uint256 expect = 1000000000000000000;
-        uint256 actual = fixedPoint.expDown(x);
-        assertEq(expect, actual);
-    }
-
-    function testExpDownRevertsWhenXGreaterThanInt256() public {
-        // check reverts when greater than int256 max
-        uint256 x = 2**255;
-        vm.expectRevert(bytes("FixedPoint: x out of bounds"));
-        fixedPoint.expDown(x);
-    }
 
 }
